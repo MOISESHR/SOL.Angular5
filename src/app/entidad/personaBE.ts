4645eb437e1444b0b755d01b8526e317fbb3cd9c
MOISESHR <moises.dev@gmail.com>
2018-03-07 15:42:19 -0500

    Primeros cambios

---
+++ src/app/entidad/personaBE.ts
@@ -0,0 +1,13 @@
+export class PersonaBE
+{
+    COD_PERSONA: number;
+    APELLIDO_PATERN: string;
+    APELLIDO_MATERN: string;
+    NOMBRES: string;
+    SEXO: string;
+    TIPO_DOCUMENTO: string;
+    FECHA_NACIMIENT: Date;
+    TELEFONO_MOVIL: string;
+    ESTADO_CIVIL: string;
+
+}
